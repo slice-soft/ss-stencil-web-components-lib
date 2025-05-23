838aba2c589d63cbdf90d9a59feebb9a858db560
jcamilofarfan <farfan1976@hotmail.com>
2025-05-23 16:02:34 -0500

    Elimina comentarios innecesarios en preview.tsx y mejora la estructura de estilos en preview-head.html para una mejor legibilidad.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,3 @@
-// .storybook/preview.tsx
 import { defineCustomElements } from '../loader/index.js';
 
-/**
- * Registers all custom elements in the Storybook preview.
- * This is useful if your components rely on other nested Stencil components.
- */
 defineCustomElements();
