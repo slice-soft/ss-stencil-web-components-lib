eedcedf25ee41fe42870c49a367d7120994a88a3
juancadev-io <farfan1976@hotmail.com>
2026-04-29 18:01:36 -0500

    feat(ss-button): add loading and accessibility label props

--- src/components.d.ts
+++ src/components.d.ts
@@ -21,6 +21,7 @@ export { Event } from "@stencil/core";
 export { FontWeight, LetterSpacing, LineHeight, TextAlign, TextTransform, TypographyColor, TypographySize, TypographyTag } from "./components/atoms/ss-typography/ss-typography";
 export namespace Components {
     interface SsButton {
+        "accessibilityLabel"?: string;
         /**
           * @default 1000
          */
@@ -39,6 +40,10 @@ export namespace Components {
         "iconPosition": IconPosition;
         "inlineStyles"?: InlineStyles;
         "label"?: string;
+        /**
+          * @default false
+         */
+        "loading": boolean;
         /**
           * After ssClick fires, button is disabled for disableDuration ms
           * @default true
@@ -242,6 +247,7 @@ declare global {
 }
 declare namespace LocalJSX {
     interface SsButton {
+        "accessibilityLabel"?: string;
         /**
           * @default 1000
          */
@@ -260,6 +266,10 @@ declare namespace LocalJSX {
         "iconPosition"?: IconPosition;
         "inlineStyles"?: InlineStyles;
         "label"?: string;
+        /**
+          * @default false
+         */
+        "loading"?: boolean;
         "onSsClick"?: (event: SsButtonCustomEvent<string | undefined>) => void;
         /**
           * After ssClick fires, button is disabled for disableDuration ms
